cab90fd156babf563ed968dfaa02417afa9fe475
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-09-24 17:21:04 +0530

    d

--- src/app/(site)/(auth)/forget-password/page.tsx
+++ src/app/(site)/(auth)/forget-password/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ForgotPassword from "@/components/Auth/ForgetPassword";
-import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
--- src/app/(site)/(auth)/signin/page.tsx
+++ src/app/(site)/(auth)/signin/page.tsx
@@ -1,5 +1,4 @@
 import Signin from "@/components/Auth/SignIn";
-import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
