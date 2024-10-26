36750a853e7447011a08c48352e2519260ff6ad3
devansh9012 <tdevansh099@gmail.com>
2024-10-26 11:16:37 +0530

    sdsd

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import { FC } from 'react';
 import Link from 'next/link';
 import { FaCheckCircle, FaEnvelope, FaHome } from 'react-icons/fa';
 import { useRouter } from "next/navigation"; // Use router to redirect
-import { toast } from "react-toastify"; // Import toast from react-toastify
 import Cookies from "js-cookie";
 
 const Success: FC = () => {
@@ -24,7 +25,7 @@ const Success: FC = () => {
         <div className="mb-10 md:mb-12 mx-auto max-w-2xl text-center px-4 md:px-6 lg:px-12 relative z-10">
           <FaCheckCircle className="text-green-500 text-5xl md:text-6xl lg:text-7xl mx-auto mb-4 md:mb-6 animate-bounce" />
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 mb-3 md:mb-4">
-            Thank You for Your Purchase!!!!!!!!!!!!!!!!
+            Thank You for Your Purchase!
           </h1>
           <p className="text-md md:text-lg lg:text-xl text-gray-600 mb-6 md:mb-8">
             Your transaction was successful, and we truly appreciate your business. Keep an eye out for a confirmation email.
@@ -35,7 +36,7 @@ const Success: FC = () => {
         <div className="flex justify-center space-x-3 md:space-x-4 mt-6 md:mt-8">
           <button onClick={handleClick} >
             <span className="inline-flex items-center bg-primary hover:bg-secondary text-secondary hover:text-white py-2 md:py-3 px-6  rounded-full text-sm md:text-base lg:text-lg font-semibold transition duration-300 transform hover:scale-105 text-center">
-              <FaHome className="mr-2 text-center" /> Back to Homes
+              <FaHome className="mr-2 text-center" /> Back to Home
             </span>
           </button>
           <Link href="/invoice" passHref>
