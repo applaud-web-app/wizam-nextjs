f5771aad1aabf9775977fd65faf7b7df753f640b
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-11-05 14:07:16 +0530

    dd

--- src/components/DashboardSidebar/index.tsx
+++ src/components/DashboardSidebar/index.tsx
@@ -1,30 +1,27 @@
-"use client"; // Ensures this runs on the client-side
+"use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation"; // Import useRouter from next/navigation
+import { usePathname } from "next/navigation";
 import { FiHome, FiEdit, FiBookOpen, FiList, FiCheckSquare, FiBook, FiBarChart } from "react-icons/fi";
 import { TfiIdBadge } from "react-icons/tfi";
 import { MdOutlinePayment, MdLogout } from "react-icons/md";
 import Cookies from "js-cookie";
 
 interface SidebarProps {
-  isSyllabusEnabled: boolean; // Whether the syllabus is enabled
-  isOpen: boolean; // Whether the sidebar is open
-  toggleSidebar: () => void; // Function to toggle the sidebar open/close
+  isSyllabusEnabled: boolean;
+  isOpen: boolean;
+  toggleSidebar: () => void;
 }
 
 export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: SidebarProps) {
-  const pathname = usePathname(); // Get the current path using usePathname
+  const pathname = usePathname();
 
-  // Helper function to apply active class if the route matches
-  const isActive = (href: string) => pathname === href ? 'bg-defaultcolor text-white' : '';
+  const isActive = (href: string) => (pathname === href ? "bg-defaultcolor text-white" : "");
 
   return (
     <>
-      {/* Sidebar overlay for mobile */}
       {isOpen && <div className="fixed inset-0 bg-black opacity-50 z-40 lg:hidden" onClick={toggleSidebar}></div>}
 
-      {/* Sidebar container */}
       <aside
         className={`fixed inset-y-0 left-0 z-50 w-64 transform bg-white text-gray-900 transition-transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
@@ -33,7 +30,6 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
         <div className="px-4 py-6">
           <h2 className="text-2xl font-bold text-gray-900 lg:hidden mb-5">Dashboard</h2>
 
-          {/* Syllabus Section */}
           <div className="mt-3 p-3 bg-gray-100 border-l-4 border-defaultcolor rounded-md">
             <div className="flex items-center">
               <FiBookOpen className="text-defaultcolor mr-2" />
@@ -45,16 +41,16 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
             <Link
               href="/dashboard/change-syllabus"
               className="flex items-center mt-2 px-4 py-2 text-sm bg-gray-200 font-medium text-gray-700 hover:bg-gray-300 rounded-md transition"
+              onClick={toggleSidebar}
             >
               <FiEdit className="mr-3" /> Change Syllabus
             </Link>
           </div>
 
-          {/* Main Navigation */}
           <div className="mt-8">
             <h3 className="text-sm font-semibold uppercase text-gray-500">Main</h3>
             <nav className="mt-4 space-y-2">
-              <Link href="/dashboard">
+              <Link href="/dashboard" onClick={toggleSidebar}>
                 <span
                   className={`flex items-center px-4 py-2 rounded-md transition ${
                     isSyllabusEnabled ? `${isActive("/dashboard")} hover:bg-gray-200` : "cursor-not-allowed opacity-50"
@@ -63,16 +59,16 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
                   <FiHome className="mr-3" /> Dashboard
                 </span>
               </Link>
-              <Link href="/dashboard/all-exams">
+              <Link href="/dashboard/all-exams" onClick={toggleSidebar}>
                 <span
                   className={`flex items-center px-4 py-2 rounded-md transition ${
-                    isSyllabusEnabled ? `${isActive("/dashboard/all-exams")} hover:bg-gray-200` : "cursor-not-allowed opacity-50"
+                    isSyllabusEnabled ? `${isActive("/dashboard/all-exams")} hover:bg-gray-200 text-gray-700` : "cursor-not-allowed opacity-50"
                   }`}
                 >
                   <FiList className="mr-3" /> Exams
                 </span>
               </Link>
-              <Link href="/dashboard/quizzes">
+              <Link href="/dashboard/quizzes" onClick={toggleSidebar}>
                 <span
                   className={`flex items-center px-4 py-2 rounded-md transition ${
                     isSyllabusEnabled ? `${isActive("/dashboard/quizzes")} hover:bg-gray-200` : "cursor-not-allowed opacity-50"
@@ -81,7 +77,7 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
                   <FiCheckSquare className="mr-3" /> Quizzes
                 </span>
               </Link>
-              <Link href="/dashboard/learn-and-practice">
+              <Link href="/dashboard/learn-and-practice" onClick={toggleSidebar}>
                 <span
                   className={`flex items-center px-4 py-2 rounded-md transition ${
                     isSyllabusEnabled ? `${isActive("/dashboard/learn-and-practice")} hover:bg-gray-200` : "cursor-not-allowed opacity-50"
@@ -90,7 +86,7 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
                   <FiBook className="mr-3" /> Learn & Practice
                 </span>
               </Link>
-              <Link href="/dashboard/my-progress">
+              <Link href="/dashboard/my-progress" onClick={toggleSidebar}>
                 <span
                   className={`flex items-center px-4 py-2 rounded-md transition ${
                     isSyllabusEnabled ? `${isActive("/dashboard/my-progress")} hover:bg-gray-200` : "cursor-not-allowed opacity-50"
@@ -102,21 +98,20 @@ export default function Sidebar({ isSyllabusEnabled, isOpen, toggleSidebar }: Si
             </nav>
           </div>
 
-          {/* Settings */}
           <div className="mt-8">
             <h3 className="text-sm font-semibold uppercase text-gray-500">Settings</h3>
             <nav className="mt-4 space-y-2">
-              <Link href="/dashboard/my-subscription">
+              <Link href="/dashboard/my-subscription" onClick={toggleSidebar}>
                 <span className={`flex items-center px-4 py-2 hover:bg-gray-200 rounded-md transition ${isActive("/dashboard/my-subscription")}`}>
                   <TfiIdBadge className="mr-3" /> My Subscriptions
                 </span>
               </Link>
-              <Link href="/dashboard/my-payments">
+              <Link href="/dashboard/my-payments" onClick={toggleSidebar}>
                 <span className={`flex items-center px-4 py-2 hover:bg-gray-200 rounded-md transition ${isActive("/dashboard/my-payments")}`}>
                   <MdOutlinePayment className="mr-3" /> My Payments
                 </span>
               </Link>
-              <Link href="/logout">
+              <Link href="/logout" onClick={toggleSidebar}>
                 <span className="flex items-center px-4 py-2 hover:bg-red-500 text-red-500 hover:text-white rounded-md transition">
                   <MdLogout className="mr-3" /> Logout
                 </span>
