98f655d27c31df847f86a7617207be1187cce802
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-11-06 16:31:15 +0530

    dd

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -2,7 +2,7 @@
 
 import { FC } from 'react';
 import Link from 'next/link';
-import { FaCheckCircle, FaEnvelope, FaHome } from 'react-icons/fa';
+import { FaCheckCircle, FaEnvelope, FaHome, FaLongArrowAltRight } from 'react-icons/fa';
 import { useRouter } from "next/navigation"; // Use router to redirect
 import Cookies from "js-cookie";
 
@@ -27,31 +27,25 @@ const Success: FC = () => {
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800 mb-3 md:mb-4">
             Thank You for Your Purchase!
           </h1>
-          <p className="text-md md:text-lg lg:text-xl text-gray-600 mb-6 md:mb-8">
-            Your transaction was successful, and we truly appreciate your business. Keep an eye out for a confirmation email.
-          </p>
+          
         </div>
 
         {/* Buttons */}
         <div className="flex justify-center space-x-3 md:space-x-4 mt-6 md:mt-8">
           <button onClick={handleClick} >
             <span className="inline-flex items-center bg-primary hover:bg-secondary text-secondary hover:text-white py-2 md:py-3 px-6  rounded-full text-sm md:text-base lg:text-lg font-semibold transition duration-300 transform hover:scale-105 text-center">
-              <FaHome className="mr-2 text-center" /> Continue
+              Continue <FaLongArrowAltRight /> 
             </span>
           </button>
-          <Link href="/invoice" passHref>
+          {/* <Link href="/invoice" passHref>
             <span className="inline-flex items-center bg-secondary hover:bg-secondary-dark text-white py-2 md:py-3 px-6  rounded-full text-sm md:text-base lg:text-lg font-semibold transition duration-300 transform hover:scale-105">
               <FaEnvelope className="mr-2" /> View Invoice
             </span>
-          </Link>
+          </Link> */}
         </div>
 
-        {/* Subtle Animation for User Engagement */}
-        <div className="mt-8 md:mt-10 animate-pulse text-center">
-          <p className="text-sm md:text-md lg:text-lg text-gray-500">
-            You will receive an email with further details shortly.
-          </p>
-        </div>
+      
+      
       </div>
 
      
