8268492e530d9efd2e9c468d77486ebc4579779b
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-11-12 11:35:50 +0530

    dd

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: './src' }); // Using 'src' as the app directory
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+
+    // Handle custom routing or other middleware if needed
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
